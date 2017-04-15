f219c461c3d137b792c70dbdd4099c9452b04a06
illusi0n455 <illusi0n455@gmail.com>
2017-04-15 18:59:57 +0300

    Add files via upload

--- homework6/2.py
+++ homework6/2.py
@@ -1,11 +1,22 @@
 import re
 
-expr = re.compile(r'fu$')
+
+def func(x):
+    try:
+        if re.split(expr, x)[1] == x:
+            return True
+        else:
+            return False
+    except:
+        return False
+
+
+expr = re.compile(r'(.*fu)')
 list1 = ['fu', 'tofu', 'snafu']
 list2 = ['futz', 'fusillade', 'functional', 'discombobulated']
 
-res1 = list(filter(lambda x: re.search(expr, x), list1))
-res2 = list(filter(lambda x: re.search(expr, x), list2))
+res1 = list(filter(func, list1))
+res2 = list(filter(func, list2))
 
 if len(res1) == len(list1) and len(res2) == 0:
     print("Well done")
